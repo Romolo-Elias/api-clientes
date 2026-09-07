1a938e92cdb3ae56d1a66c53874f3704d797010d
Romolo Elias <romolopsn2004@gmail.com>
2026-09-07 02:27:24 -0300

    model, controller e routes de cliente

--- src/controllers/clienteController.js
+++ src/controllers/clienteController.js
@@ -23,7 +23,31 @@ class clienteController {
 
     static async criarCliente (req, res){
         try{
+            const { nome, email, uf } = req.body;
+            const novoCliente = await cliente.create({nome, email, uf})
+            res.status(201).json(novoCliente)
+        } catch (erro){
+            res.status(500).json({message: `${erro.message} Falha na requisição`})
+        }
+    }
 
+    static async atualizarCliente (req, res){
+        try{
+            const id = req.params.id
+            const { nome, email, uf } = req.body;
+            const clienteAtualizado = await cliente.findByIdAndUpdate(id, {nome, email, uf}, { new: true, runValidators: true })
+            res.status(200).json(clienteAtualizado)
+
+        } catch (erro){
+            res.status(500).json({message: `${erro.message} Falha na requisição`})
+        }
+    }
+
+    static async deletarCliente (req, res){
+        try{
+            const id = req.params.id
+            const clienteApagado = await cliente.deleteOne({_id: id})
+            return res.status(204).end()
         } catch (erro){
             res.status(500).json({message: `${erro.message} Falha na requisição`})
         }
--- src/routes/clientesRoutes.js
+++ src/routes/clientesRoutes.js
@@ -5,3 +5,8 @@ const routes = express.Router()
 
 routes.get("/clientes", clienteController.listarClientes)
 routes.get("/clientes/:id", clienteController.listarCliente)
+routes.post("/clientes", clienteController.criarCliente)
+routes.put("/clientes/:id", clienteController.atualizarCliente)
+routes.delete("/clientes/:id", clienteController.deletarCliente)
+
+export default routes
